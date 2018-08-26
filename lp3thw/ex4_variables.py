6c9674fdb6ddbb43529507662ea02089f943c88c
Karo Stoltzenburg <kstoltzenburg@gmail.com>
2018-08-26 15:17:50 +0100

    Add exercise 4 - variables

---
+++ lp3thw/ex4_variables.py
@@ -0,0 +1,46 @@
+# setup variables
+# how many cars available?
+cars = 100
+# how many spaces per car?
+# NOTE: This assumes available spaces are the same for all cars
+# space_in_a_car = 4.0
+space_in_a_car = 4
+# how many drivers?
+drivers = 30
+# how many passengers?
+passengers = 90
+# given available drivers, how many cars will not be driven?
+cars_not_driven = cars - drivers
+# given available drivers, how many cars will be driven?
+# NOTE: this will be incorrect, if we have more drivers then cars; rather
+# if drivers >= cars; cars_driven = drivers; else cars_driven = cars
+cars_driven = drivers
+# overall available spaces in cars for carpooling
+carpool_capacity = cars_driven * space_in_a_car
+# How many people per car for carpooling?
+average_passengers_per_car = passengers / cars_driven
+
+# -- Study drill
+# average_passengers_per_car = car_pool_capacity / passenger
+# Will give an error: "name 'car_pool_capacity' is not defined"
+# I'd be using, in line 8/9, a variable that has not been defined before,
+# python doesn't know what this is supposed to be
+# Presumably a typo - I'm defining in line 7/8: 'carpool_capacity'
+# but here, I'm trying to use it as           : 'car_pool_capacity'
+# --
+
+# print information
+print("There are", cars, "cars available.")
+print("There are only", drivers, "drivers available.")
+print("There will be", cars_not_driven, "empty cars today.")
+print("We can transport", carpool_capacity, "people today.")
+print("We have", passengers, "to carpool today.")
+print("We need to put about", average_passengers_per_car, "in each car.")
+
+# -- Study drill
+# Is using 4.0 for space_in_a_car necessary?
+# - frankly, I don't see it making a difference or being important.
+# Am I certain? No. 4.0 being a floating point number might make it
+# more exact in calculations, but I tried with different values,
+# to put 2.66... people in each car, and it had the same results.
+# --
--- lp3thw/notes.md
+++ lp3thw/notes.md
@@ -15,7 +15,7 @@ Why do I have to read code backward?
 * It's a trick to make your brain not attach meaning to each part of the code, and doing that makes you process each piece exactly. This catches errors and is a handy error-checking technique.
 * Did you find more mistakes? Fix them.
 
-## Exercise 3 (Numbers)
+## Exercise 3 (Numbers & Math)
 
 How does % work?
 
@@ -28,4 +28,28 @@ How does % work?
 What is the order of operations?
 * In the United States we use an acronym called PEMDAS which stands for Parentheses Exponents Multiplication Division Addition Subtraction. That's the order Python follows as well. The mistake people make with PEMDAS is to think this is a strict order, as in "Do P, then E, then M, then D, then A, then S." The actual order is you do the multiplication and division (M&D) in one step, from left to right, then you do the addition and subtraction in one step from left to right. So, you could rewrite PEMDAS as PE(M&D)(A&S).
 * In German: Punkt vor Strich, von links nach rechts.
+
+## Exercise 4 (Variables)
+
+Tricks taught so far:
+* write comments to explain to yourself in english what it should do
+* read your .py file backwards
+* read your .py file out loud, saying even the characters
+
+
+Printing using comma separated values will add spaces around the values:
+
+```
+>>> print("There are", cars, "cars available.")
+>>> There are 100 cars available. 
+```
+
+If you don't want to have the extra spaces, you can use string concatenation:
+
+```
+>>> print("There are " + str(cars) + " cars available.")
+>>> There are 100 cars available. 
+```
+
+For more information, see: https://stackoverflow.com/questions/28669459/how-to-print-variables-without-spaces-between-values#
 
